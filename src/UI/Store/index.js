17b950ab75f116100bb01735e6345104ec68306b
Patrick Faramaz <faramazpat@gmail.com>
2020-03-16 15:45:52 +0100

    set window function show modale

--- src/UI/Store/index.js
+++ src/UI/Store/index.js
@@ -34,7 +34,7 @@ export default (options: ?OptionsGdpr, locale: string, messages: Object) => {
     listenClick(store);
   }
   // set global method for change setting cookie with click on link
-  global._gdpr_showModal = () => {
+  window._gdpr_showModal = () => {
     store.setState({showModal: true});
   };
 
