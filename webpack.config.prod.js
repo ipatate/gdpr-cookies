825769ec5c3bb89f5c16b827b8b0f75ded2ab785
Patrick Faramaz <faramazpat@gmail.com>
2020-03-16 19:41:54 +0100

    export init main for give the hand on init gdpr

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,6 +11,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'gdpr-cookies.js',
+    library: 'initGdprCookie',
+    libraryTarget: 'umd',
   },
   module: {
     rules: [
